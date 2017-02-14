443026411ad4179264691b0433bca9ea35cd5092
Alex Tsilingiris <alex19924@gmail.com>
2017-02-15 00:17:08 +0200

    closes #5

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/spring/ClassShouldContainOnlyOneComponentAnnotation.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/spring/ClassShouldContainOnlyOneComponentAnnotation.java
@@ -1,6 +1,5 @@
 package net.sourceforge.pmd.lang.java.rule.spring;
 
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTMarkerAnnotation;
 import net.sourceforge.pmd.lang.java.ast.ASTTypeDeclaration;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
@@ -10,35 +9,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-//Only for single class or interface declaration per file
-public class ClassNameShouldMatchAnnotation extends AbstractJavaRule
+public class ClassShouldContainOnlyOneComponentAnnotation extends AbstractJavaRule
 {
 
     @Override
     public Object visit ( ASTTypeDeclaration typeDeclaration, Object data )
     {
         List<String> annotationIdentifiersToCheck = new ArrayList<>();
+        annotationIdentifiersToCheck.add( "RestController" );
         annotationIdentifiersToCheck.add( "Controller" );
         annotationIdentifiersToCheck.add( "Service" );
         annotationIdentifiersToCheck.add( "Repository" );
+        annotationIdentifiersToCheck.add( "Component" );
 
         List<String> annotationsAsString = typeDeclaration.findDescendantsOfType( ASTMarkerAnnotation.class )
                 .stream().map( a -> a.jjtGetLastToken().toString() ).collect( Collectors.toList() );
 
-        String classOrInterfaceIdentifier = typeDeclaration.findDescendantsOfType( ASTClassOrInterfaceDeclaration.class )
-                .get( 0 ).getImage();
-
-        annotationIdentifiersToCheck.forEach( anId ->
+        int found = 0;
+        for ( String toCheck : annotationIdentifiersToCheck )
         {
-            if ( annotationsAsString.contains( anId ) )
+            if ( annotationsAsString.contains( toCheck ) )
             {
-                if ( !classOrInterfaceIdentifier.contains( anId ) )
-                {
-                    super.addViolation( data, typeDeclaration );
-                }
+                found++;
             }
-        } );
+        }
+        if ( found > 1 )
+        {
+            super.addViolation( data, typeDeclaration );
+        }
         return super.visit( typeDeclaration, data );
     }
-
 }
--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/spring/SpringRulesTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/spring/SpringRulesTest.java
@@ -14,6 +14,7 @@ public class SpringRulesTest extends SimpleAggregatorTst
         addRule( RULESET, "ClassNameShouldMatchAnnotation" );
         addRule( RULESET, "VariableNameShouldMatchAnnotatedClass" );
         addRule( RULESET, "EntityShouldContainNoArgsConstructor" );
+        addRule( RULESET, "ClassShouldContainOnlyOneComponentAnnotation" );
     }
 
 }
